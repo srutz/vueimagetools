bb908a98439ce77727552aed322418c742f9c23e
stepan.rutz <stepan.rutz@gmx.de>
2026-05-28 13:00:55 +0200

    added carpanel component

---
+++ src/components/CardPanel.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface PropsType {  // definiere unsere eigenschaften
+    heading: string
+    footer?: string
+    collapsed?: boolean
+}
+const { heading, footer = "Default footer text" } = defineProps<PropsType>()
+</script>
+<template>
+    <div class="flex flex-col gap-4 border border-gray-300 shadow-xl p-4 m-2 rounded-xl">
+        <div class="font-semibold text-xl uppercase tracking-widest">{{ heading }}</div>
+        <template v-if="!collapsed">
+            <slot ></slot>
+            <div class="text-right pt-2 text-xs text-gray-500 border-gray-300 border-t">
+                {{ footer }}
+            </div>
+        </template>
+    </div>
+</template>
--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import CardPanel from "../CardPanel.vue";
+const collapsed = ref(false)
+</script>
 
 <template>
-  <main>Home</main>
+  <main>
+    <CardPanel heading="Preferences" :collapsed="collapsed" footer="Manage your preferences">
+      Lorem ipsum lorem tada.
+      Lorem ipsum lorem tada.
+      Lorem ipsum lorem tada,
+      </CardPanel>
+      <button class="px-2 py-1 border border-gray-400 cursor-pointer"
+        @click="collapsed = !collapsed">Toggle</button>
+  </main>
 </template>
